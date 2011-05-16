f8f8a6febb33ba78f9ecc39836671c31c7a7a652
Rob Morgan <robbym@gmail.com>
2011-05-16 22:12:26 +0800

    Adding jQuery plugins

---
+++ jquery.danodropdown.js
@@ -0,0 +1,60 @@
+/*
+
+	jQuery DanoDropdown Plugin 0.1
+	
+	Copyright (c) 2011 EDM.DJ
+	
+	Licensed under the MIT license:
+	http://www.opensource.org/licenses/mit-license.php
+
+	http://edm.dj/site/contact
+
+*/
+
+(function($) {
+	
+	jQuery.fn.danoDropdown = function(options) {
+		
+		// there's no settings for this component atm
+		var settings = jQuery.extend({}, options);
+		
+		$(this).each(function() {
+			var source = $(this);
+			var sourceParent = $(this).parent().attr("id");
+            var selected = $(this).find("option[selected]");
+            var options = $("option", source);
+            
+            var newDropTarget = ("target-" + sourceParent);
+            
+            // Hide the existing select box
+            source.css({
+            	"position": "absolute",
+            	"top": "-9999px"
+            });
+            
+            $("#" + sourceParent).append('<div id="' + newDropTarget + '" class="drop-wrap"></div>');
+            $("#" + newDropTarget).append('<div class="text"><div class="text-wrap">' + selected.text() + '</div></div>');
+            $("#" + newDropTarget).append('<ul></ul>');
+
+            options.each(function(){
+                $("#" + newDropTarget + " ul").append('<li>' + 
+                    $(this).text() + '</li>');
+            });
+            
+            $("#" + newDropTarget + " .text").click(function() {
+                $("#" + newDropTarget + " ul").toggle();
+            });
+            
+            $("#" + newDropTarget + " li").click(function() {
+                var text = $(this).html();
+                $("#" + newDropTarget + " .text-wrap").html(text);
+                source[0].selectedIndex=$(this).index("#" + newDropTarget + " li");
+                $("#" + newDropTarget + " ul").hide();
+            });
+		});
+		
+		return this;
+		
+	};
+			
+})(jQuery);
